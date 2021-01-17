62988c4fa5fba6f109cbf8a18e837286a4624515
Mus'ab Husaini <579203+musabhusaini@users.noreply.github.com>
2021-01-17 12:50:13 +0300

    add package setup files

---
+++ setup.py
@@ -0,0 +1,24 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+setup(
+    name="aioredis-models",
+    version="0.1.0",
+    description="Model Redis data structures",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/qcrisw/aioredis-models",
+    author="QCRI Software Group",
+    author_email="qcriswgroup@hbku.edu.qa",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords = ['redis', 'asyncio', 'data-structures', 'models'],
+    packages=find_packages(exclude=("tests",)),
+    install_requires=["aioredis==1.3.1"],
+)
